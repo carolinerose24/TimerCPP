9fc0ba23e59a6ec7082c101a4f06d17b0c9a199d
Caroline Jarman <carolinerosejarman@gmail.com>
2019-02-01 08:20:06 -0700

    Initial Commit

---
+++ TimerCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TimerCPP
+//
+//  Created by Jarman, Caroline on 2/1/19.
+//  Copyright © 2019 ctec. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
